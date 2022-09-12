751f7d89692c46d32da54432d14c9ee2400a1b24
JooMin Choi <joomin.choi@clearscore.com>
2022-09-12 11:35:41 +0100

    first commit

---
+++ WeSplit_Implementation/ContentView.swift
@@ -0,0 +1,113 @@
+//
+//  ContentView.swift
+//  WeSplit_Implementation
+//
+//  Created by JooMin Choi on 23/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 15
+    @FocusState private var amountIsFocused: Bool
+    
+    let currencyFormatter = FloatingPointFormatStyle<Double>.Currency(code: Locale.current.currencyCode ?? "USD")
+    
+//    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var grandTotal: Double {
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        
+        return checkAmount + tipValue
+    }
+    
+    var totalPerPerson: Double {
+//        calculate the total per person here
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: currencyFormatter)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                } header: {
+                    Text("Subtotal")
+                }
+                
+//                Section {
+//                    Picker("Tip percentage", selection: $tipPercentage) {
+//                        ForEach(tipPercentages, id: \.self) {
+//                            Text($0, format: .percent)
+//                        }
+//                    }
+//                    .pickerStyle(.segmented)
+//                } header: {
+//                    Text("How much tip would you like to leave?")
+//                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<101) {
+                            Text("\($0)%")
+                        }
+                    }
+                } header: {
+                    Text("How much tip would you like to leave?")
+                }
+                 
+                Section {
+                    Text(grandTotal, format: currencyFormatter)
+                } header: {
+                    Text("Grand total")
+                }
+                
+                Section {
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                } header: {
+                    Text("Split between how many people?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                } header: {
+                    Text("Total per person")
+                }
+                
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit_Implementation/WeSplit_ImplementationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_ImplementationApp.swift
+//  WeSplit_Implementation
+//
+//  Created by JooMin Choi on 23/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_ImplementationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
